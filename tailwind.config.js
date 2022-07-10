72ea9b7261aa0c7564ab926b01e80cb741046260
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-07-10 13:57:42 +0200

    adjust setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
 		"./src/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark-cyan': 'hsl(158, 36%, 37%)',
+				'cream': 'hsl(30, 38%, 92%)',
+				'very-dark-blue': 'hsl(212, 21%, 14%)',
+				'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+				'white': 'hsl(0, 0%, 100%)'
+			}
+		},
+		fontFamily: {
+			montserrat: ["Montserrat"],
+			fraunces: ["Fraunces"]
+		}
 	},
 	plugins: [],
 }
